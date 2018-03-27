2925fda6f25605d2f4a1fa6ee712c87f3d2d789a
Farangis Hakimi <34238647+farangiis@users.noreply.github.com>
2018-03-27 10:09:30 +0200

    Add files via upload

---
+++ src/AppComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class AppComponent extends Component {
+   render() {
+      return (
+         <div>
+            Hello World
+         </div>
+      )
+   }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import AppComponent from './AppComponent';
+
+render(<AppComponent />, document.getElementById('app'));
+
