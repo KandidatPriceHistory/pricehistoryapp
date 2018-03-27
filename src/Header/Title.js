ac73c641f9e74c09da26f1eaf3ecc4d4b71f8e5d
Farangis Hakimi <34238647+farangiis@users.noreply.github.com>
2018-03-27 13:48:21 +0200

    Kaos

---
+++ src/Header/Title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Title extends React.Component {
+     render() {
+      return (
+         <h1>
+            Välkommen
+         </h1>
+      );
+   }
+
+
+}
+  
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import AppComponent from './AppComponent';
+import AppComponent from './Component/AppComponent';
+
+
 
 render(<AppComponent />, document.getElementById('app'));
-
