68251d0020bfd17a64cb679891e13c3e4f5cfeb2
Farangiis <34238647+farangiis@users.noreply.github.com>
2018-04-15 21:29:54 +0200

    Gjorde ett dummy test

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import AppComponent form './components';
+import { shallow } from 'enzyme';
 // in app/src/setupTests.js file
 const Enzyme = require('enzyme');
 // this is where we reference the adapter package we installed
@@ -5,3 +8,9 @@ const Enzyme = require('enzyme');
 const EnzymeAdapter = require('enzyme-adapter-react-15');
 // This sets up the adapter to be used by Enzyme
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
