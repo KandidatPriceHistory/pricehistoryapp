aed16562c5cdce8e15c82340ae2827a51ba74c9c
Farangiis <34238647+farangiis@users.noreply.github.com>
2018-04-18 14:25:24 +0200

    Ändringar

---
+++ src/app/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import AppComponent from './components/AppComponent';
+
+
+render(<AppComponent />, document.getElementById('app'));
+//translate: renderar AppComponent i html koden med id "app"
