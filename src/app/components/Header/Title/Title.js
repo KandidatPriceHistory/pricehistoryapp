0a9e284ba2b1a32f05353686cd190a09c7dcdc62
alMcpal <alvalarsson1995@gmail.com>
2018-04-19 14:09:26 +0200

    small scss stuff

--- src/app/components/Header/Title/Title.js
+++ src/app/components/Header/Title/Title.js
@@ -5,7 +5,7 @@ export default class Title extends React.Component {
    render() {
      return (
        <div>
-          <h1 className = "titleText"> PriceHistory </h1>
+          <h1 id = "titleText"> PriceHistory </h1>
         </div>
       );
    }
