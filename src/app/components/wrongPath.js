1dcbc9152f9b51e49860615ccf1dc3656f5f353f
Sofia <ellegren.sofia@gmail.com>
2018-04-20 15:40:54 +0200

    Added a page not found page and inserted a picture to the product info box

---
+++ src/app/components/wrongPath.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import Footer from './Footer/Footer';
+
+export default class wrongPath extends Component {
+   render() {
+      return (
+         <div>
+              <img id="wrongPicture" src="https://wersm.com/wp-content/uploads/2013/07/404.png" />
+         </div>
+      );
+   }
+}
