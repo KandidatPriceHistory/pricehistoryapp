553608a67ade76c851ce22122e27f7715b120d32
Farangiis <34238647+farangiis@users.noreply.github.com>
2018-04-20 15:21:43 +0200

    Har gjort en dummy-graf
    Nu kan man se något i productPriceHistory

--- src/app/components/Graph/Graph.js
+++ src/app/components/Graph/Graph.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styles from "./Graph.scss";
-import { LineChart, Line } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend  } from 'recharts';
 
 
 
@@ -10,13 +10,13 @@ export default class Graph extends Component {
     super(props);
     this.state = {
       data:[
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100}
+      {name: 'November', uv: 4000},
+      {name: 'December', uv: 4000},
+      {name: 'Januari', uv: 5030},
+      {name: 'Februari', uv: 2780},
+      {name: 'Mars', uv: 1890},
+      {name: 'April', uv: 50},
+      {name: 'Maj', uv: 3490}
     ],
     }
   }
@@ -26,8 +26,12 @@ export default class Graph extends Component {
       return (
         <div className = "graphDiv">
         <h4 id= "temporaryGraphTitle "> This is the Graph component! </h4>
-        <LineChart width={400} height={400} data={this.state.data}>
+        <LineChart width={600} height={400} data={this.state.data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
         <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <CartesianGrid stroke ="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="name" />
+        <YAxis />
+         <Tooltip />
         </LineChart>
         </div>
       );
--- src/app/components/productPriceHistory.js
+++ src/app/components/productPriceHistory.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import ProductList from './Product/ProductList';
+import Header from './Header/Header.js';
+import Graph from './Graph/Graph.js';
 
 export default class Product extends Component {
    render() {
       return (
          <div>
-              <ProductList />
+               <Header />
+              <Graph/>
          </div>
       );
    }
