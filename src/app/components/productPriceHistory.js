418f6d5cd4f15c2d1c1caf017f97cee2c7f43877
Farangiis <34238647+farangiis@users.noreply.github.com>
2018-04-20 16:49:46 +0200

    Fixade bug

--- src/app/components/productPriceHistory.js
+++ src/app/components/productPriceHistory.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import ProductList from './Product/ProductList';
 import Header from './Header/Header.js';
 import Graph from './Graph/Graph.js';
-import ProductItemBox from './Product/ProductItemBox';
+import ProductItemBox from './Product/ProductInfoBox';
 
 export default class productPriceHistory extends Component {
    render() {
       return (
          <div>
-
                 <Header />
               <ProductItemBox />
-       
               <Graph/>
          </div>
       );
