baef8b181e468ba978149aece96fb1fda55c5b8c
Farangiis <34238647+farangiis@users.noreply.github.com>
2018-04-22 22:50:26 +0200

    Småfix med grafen
    Mobilanpassad version

--- src/app/components/Graph/Graph.js
+++ src/app/components/Graph/Graph.js
@@ -10,12 +10,12 @@ export default class Graph extends Component {
     super(props);
     this.state = {
       data:[
-      {name: 'November', uv: 4000},
-      {name: 'December', uv: 4000},
-      {name: 'Januari', uv: 5030},
-      {name: 'Februari', uv: 2780},
-      {name: 'Mars', uv: 1890},
-      {name: 'April', uv: 50},
+      {name: 'Nov', uv: 4000},
+      {name: 'Dec', uv: 4000},
+      {name: 'Jan', uv: 5030},
+      {name: 'Feb', uv: 2780},
+      {name: 'Mar', uv: 1890},
+      {name: 'Apr', uv: 50},
       {name: 'Maj', uv: 3490}
     ],
     }
@@ -26,7 +26,7 @@ export default class Graph extends Component {
       return (
         <div className = "graphDiv">
         <h4 id= "temporaryGraphTitle "> This is the Graph component! </h4>
-        <LineChart width={600} height={400} data={this.state.data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+        <LineChart width={400} height={400} data={this.state.data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
         <Line type="monotone" dataKey="uv" stroke="#8884d8" />
         <CartesianGrid stroke ="#ccc" strokeDasharray="5 5" />
         <XAxis dataKey="name" />
