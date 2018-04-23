121e83de7bce65fc185cf00b1d78d76519a52c19
Farangiis <34238647+farangiis@users.noreply.github.com>
2018-04-23 09:49:36 +0200

    Håller på att använda redux

--- src/app/components/Graph/Graph.js
+++ src/app/components/Graph/Graph.js
@@ -10,13 +10,13 @@ export default class Graph extends Component {
     super(props);
     this.state = {
       data:[
-      {name: 'Nov', uv: 4000},
-      {name: 'Dec', uv: 4000},
-      {name: 'Jan', uv: 5030},
-      {name: 'Feb', uv: 2780},
-      {name: 'Mar', uv: 1890},
-      {name: 'Apr', uv: 50},
-      {name: 'Maj', uv: 3490}
+      {name: 'Nov', price: 4000},
+      {name: 'Dec', price: 4000},
+      {name: 'Jan', price: 5030},
+      {name: 'Feb', price: 2780},
+      {name: 'Mar', price: 1890},
+      {name: 'Apr', price: 50},
+      {name: 'Maj', price: 3490}
     ],
     }
   }
@@ -27,7 +27,7 @@ export default class Graph extends Component {
         <div className = "graphDiv">
         <h4 id= "temporaryGraphTitle "> This is the Graph component! </h4>
         <LineChart width={400} height={400} data={this.state.data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <Line type="monotone" dataKey="price" stroke="#8884d8" />
         <CartesianGrid stroke ="#ccc" strokeDasharray="5 5" />
         <XAxis dataKey="name" />
         <YAxis />
