470f1bf4e7cf426b185356688834e861a6c99a01
Farangiis <34238647+farangiis@users.noreply.github.com>
2018-04-24 11:54:18 +0200

    Uppdaterar

--- src/app/components/Product/ProductInfoBox.js
+++ src/app/components/Product/ProductInfoBox.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import styles from "./Product.scss"
+import ProductInfoBoxText from "./ProductInfoBoxText";
 
 export default class ProductItemBox extends React.Component {
   render() {
      return (
         <div class = "productInfoBox">
           <img id ="productPicture" src="https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone/x/iphone-x-select-2017?wid=189&hei=376&fmt=png-alpha&.v=1504378258086" />
+      
         </div>
      );
   }
