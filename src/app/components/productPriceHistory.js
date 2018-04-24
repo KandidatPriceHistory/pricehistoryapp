81d0ad34c8fe5c8d4e4b3e2471764edf8d786dbc
alMcpal <alvalarsson1995@gmail.com>
2018-04-24 14:26:47 +0200

    fixar med homepage-part 2

--- src/app/components/productPriceHistory.js
+++ src/app/components/productPriceHistory.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './Header/Header.js';
 import Graph from './Graph/Graph.js';
 import ProductItemBox from './Product/ProductInfoBox';
+import Footer from './Footer/Footer';
 
 export default class productPriceHistory extends Component {
 
@@ -11,6 +12,7 @@ export default class productPriceHistory extends Component {
                 <Header />
                 <ProductItemBox />
                 <Graph />
+                <Footer />
             </div>
         );
     }
