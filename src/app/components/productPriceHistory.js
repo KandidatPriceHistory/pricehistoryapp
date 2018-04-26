27d047aa84ea18b23423f9a3c0a9ee1692eb2099
alMcpal <alvalarsson1995@gmail.com>
2018-04-26 09:09:30 +0200

    ändrat felet i förra mergen

--- src/app/components/productPriceHistory.js
+++ src/app/components/productPriceHistory.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header/Header.js';
 import Graph from './Graph/Graph.js';
-import ProductItemBox from './Product/ProductInfoBox';
 import Footer from './Footer/Footer';
 
 export default class productPriceHistory extends Component {
@@ -10,7 +9,6 @@ export default class productPriceHistory extends Component {
         return (
             <div>
                 <Header />
-
                 <Graph />
                 <Footer />
             </div>
