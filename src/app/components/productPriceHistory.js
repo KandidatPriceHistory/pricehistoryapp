e3624138f49e869fff62cf576c9a14d4bb8e2656
Sofia <ellegren.sofia@gmail.com>
2018-05-03 13:15:45 +0200

    product box added to the graph view

--- src/app/components/productPriceHistory.js
+++ src/app/components/productPriceHistory.js
@@ -3,13 +3,17 @@ import Header from './Header/Header.js';
 import Graph from './Graph/Graph.js';
 import Footer from './Footer/Footer';
 import { connect } from 'react-redux';
-import reducers from "../reducers/index.js";
+import reducers from "../reducers/index.js"
+
+import ProductInfoBox from './Product/ProductInfoBox';
+
 
 class productPriceHistory extends Component {
     render() {
         return (
             <div>
                 <Header />
+                <ProductInfoBox product={this.props.selectedProduct}/>
                 <Graph />
                 <Footer />
             </div>
@@ -19,7 +23,7 @@ class productPriceHistory extends Component {
 }
 
 const mapStateToProps = state => {
-  return { graph: state.graph }
+  return { graph: state.graph, selectedProduct: state.selectedProduct }
 }
 
 export default connect(mapStateToProps)(productPriceHistory);
