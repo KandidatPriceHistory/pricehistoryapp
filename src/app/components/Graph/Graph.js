1049a998e2ddd54beabff0c88b4a7249e760e7a9
Farangiis <34238647+farangiis@users.noreply.github.com>
2018-05-03 11:21:58 +0200

    Nu syns grafen som den ska

--- src/app/components/Graph/Graph.js
+++ src/app/components/Graph/Graph.js
@@ -12,13 +12,14 @@ const mapStateToProps = state => {
  class Graph extends React.Component {
 
    render() {
+     console.log(this.props.graph);
       return (
         <div className = "graphDiv">
           <h4 id= "temporaryGraphTitle"> This is the Graph component! </h4>
             <LineChart width={400} height={400} data={this.props.graph} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-              <Line type="monotone" dataKey={this.props.graph.price} stroke="#8884d8" />
+              <Line type="monotone" dataKey="price" stroke="#8884d8" />
               <CartesianGrid stroke ="#ccc" strokeDasharray="5 5" />
-              <XAxis dataKey={this.props.graph.month} />
+              <XAxis dataKey="month" />
               <YAxis />
               <Tooltip />
             </LineChart>
