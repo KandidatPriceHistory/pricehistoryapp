edd404d4646288c0a2448e6c479ed2c0573ec0b9
Farangiis <34238647+farangiis@users.noreply.github.com>
2018-05-03 11:28:13 +0200

    Uppdatering men text

--- src/app/components/Graph/Graph.js
+++ src/app/components/Graph/Graph.js
@@ -15,7 +15,6 @@ const mapStateToProps = state => {
      console.log(this.props.graph);
       return (
         <div className = "graphDiv">
-          <h4 id= "temporaryGraphTitle"> This is the Graph component! </h4>
             <LineChart width={400} height={400} data={this.props.graph} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
               <Line type="monotone" dataKey="price" stroke="#8884d8" />
               <CartesianGrid stroke ="#ccc" strokeDasharray="5 5" />
