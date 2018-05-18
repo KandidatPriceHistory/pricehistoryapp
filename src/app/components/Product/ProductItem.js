0f87c1b0d7c1c623e4117271f805cd076b969a45
Sofia <ellegren.sofia@gmail.com>
2018-05-18 13:39:18 +0200

    max and min price deleted

--- src/app/components/Product/ProductItem.js
+++ src/app/components/Product/ProductItem.js
@@ -10,9 +10,6 @@ const Product = ({ product }) => (
       <div className= "productnameDiv">
         <h3 id="productName"> {product.name} </h3>
       </div>
-      <div id="descriptionHome">
-        <p> Current price from {product.minPrice.value} to {product.maxPrice.value} {product.minPrice.currency}</p>
-      </div>
 
       <Link to = {`/product/${product.id}`}>
         <button type="button" className="goToRetailersButton"> Go to retailers </button>
