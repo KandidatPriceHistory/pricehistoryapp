ffe86a163eb3ba6f2837a5afbf8bfb7325ed63c8
alMcpal <alvalarsson1995@gmail.com>
2018-05-22 11:26:34 +0200

    tagit bort sorteringsfunktion som Sofia fixade istället

--- src/app/components/Graph/Graph.js
+++ src/app/components/Graph/Graph.js
@@ -6,15 +6,7 @@ import { connect } from "react-redux";
 
  export default class Graph extends React.Component {
 
-   componentWillMount(){
-     var graphDates = this.props.graph;
-     sortGraphDates(graphDates);
-     //console.log("COMPONENT WILL MOUNT", graphDates);
-   };
-
-   render(graphDates) {
-     console.log("INSIDE RENDER",graphDates);
-     /*
+   render() {
       return (
         <div className = "graphDiv">
         <LineChart className = "theGraph" width={500} height={250} data={graphDates}>
@@ -26,35 +18,5 @@ import { connect } from "react-redux";
             </LineChart>
         </div>
       );
-      */
    }
 }
-
-
-function sortGraphDates(graphDates){
-  console.log("INSIDE SORT FUNCTION");
-  graphDates.sort(function(a,b){
-  // Turn your strings into dates, and then subtract them
-  // to get a value that is either negative, positive, or zero.
-  return new Date(b.date) - new Date(a.date);
-});
-  //for (i=0; i<length.graphData;i++){
-    //var date = graphData[i].date;
-    //var slicedDate=date.slice(0,9);
-    //slicedDate= slicedDate.split('-').join('');
-    //slicedDate = parseInt(slicedDate);
-
-  //};
-  return console.log("SORTERAD:",graphDates,"!!!!!!!!!!");
- };
-
- /*  graphDates.sort(function(a,b){
-     return a.data.date.getTime() - b.data.date.getTime()
-   });*/
-
-
-//array.sort(function(a,b){return a.data.date.getTime() - b.data.date.getTime()});
-
-//gör get req till pricehistory i postman så kan
-
-// export default connect(mapStateToProps)(Graph);
